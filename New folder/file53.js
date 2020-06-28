d0e339add91e2f74ad789ac85a589ca2dac4ba22
parvez hossen <parvezpatwery6023@gmail.com>
2020-06-28 07:56:44 -0700

    js file pushed

---
+++ New folder/file53.js
@@ -0,0 +1,54 @@
+console.log("this file 53 on learn web developer");
+let name = "parvez";
+let name1 = "foysal";
+let name2 = "nasir";
+let name3 = "shakhawat";
+console.log(name + " is a good boy");
+console.log(name1 + " is a good boy");
+console.log(name2 + " is a good boy");
+console.log(name3 + " is a good boy ");
+// ami oporer code gula sob akbare function diya atobar nah like korte parbo tar system
+function greet(pk, ) {
+    console.log(pk + "is do everytime heardwork")
+}
+let pk4 = "nadim";
+let pk5 = "sumon"
+let pk6 = "mamun"
+let pk7 = "khaled"
+greet(pk4)
+greet(pk5)
+greet(pk6)
+greet(pk7)
+
+// jodi ami ectra word add kotre chi function ar vetore taholer j funuctin use korte hobe seta show kortest 
+
+
+// this tag is function tag it call the function tag function ar modde joto kushi toto call korte parve 
+function extra(nam, title = "he is a engineer") {
+
+    console.log(nam + " is a criminal")
+    console.log(title + "" + " but good boy");
+
+}
+
+
+
+function sum(a, b, c, t, h, o) {
+    let d = a + b + c + t + h + o;
+    return d;
+    // return ar por ar kono function tag add korajabe nah 
+}
+let nam4 = "nadim"
+let nam5 = "sojib"
+let nam6 = "akash"
+let nam7 = "dider"
+let title = "denger"
+extra(nam4, title);
+extra(nam5, title);
+extra(nam6, title);
+extra(nam7);
+
+let returnVal = sum(1, 2, 3, 4, 5, 6);
+console.log(returnVal)
+
+// return value jebabe kaj kore seta holo tomi fast a sum ar modde joto bar pojontto velue change korte chaw setar nub dibe tarpo function sum a giya ki ki word dibe seta select koro tarpor oi same word le ar modde diye return ver korte parba
