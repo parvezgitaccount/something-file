bc3d7f05baa3695301fca9c1ac519f7f16e51fae
parvez hossen <parvezpatwery6023@gmail.com>
2020-07-05 07:11:21 -0700

    this custom brekent folder67 file

---
+++ folder67/index.js
@@ -0,0 +1,49 @@
+// costom beckent file how to work
+
+
+const http = require('http');
+const fs = require('fs');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+// const { url } = require('inspector');
+const home = fs.readFileSync("folder67/home.html");
+const about = fs.readFileSync("folder67/about.html");
+const services = fs.readFileSync("folder67/services.html");
+const contact = fs.readFileSync("folder67/contact.html");
+
+
+
+
+
+const server = http.createServer((req, res) => {
+    console.log(req.url);
+    url = req.url;
+
+
+
+
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+
+    if (url == '/home.html') {
+        res.end(home);
+    } else if (url == '/about.html') {
+        res.end(about);
+    } else
+    if (url == '/services.html') {
+        res.end(services);
+    } else if (url == '/contact.html') {
+        res.end(contact);
+
+    } else {
+        res.statusCode = 404;
+        res.end("<h1>404 is not found</h1>");
+    }
+
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
